2b1b7345a0ab6177586b2519e8c4409271485b93
7vtr0x7 <7vtrox7@gmail.com>
2025-02-15 03:26:19 +0530

    feat: socket.io

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { config } from "dotenv";
+import { Server } from "socket.io";
+
 import { initializeDatabase } from "./db/db.connection.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
@@ -11,7 +13,7 @@ config();
 const app = express();
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -22,6 +24,39 @@ app.use("/api/chat", chatRoutes);
 initializeDatabase();
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
+});
+
+const io = new Server(server, { cors: { origin: process.env.FRONTEND_URL } });
+
+let onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  console.log("New user connected:", socket.id);
+
+  socket.on("join", (userId) => {
+    onlineUsers.set(userId, socket.id);
+    console.log(`User ${userId} is online`);
+  });
+
+  socket.on("sendMessage", ({ sender, receiver, message }) => {
+    console.log(`Message from ${sender} to ${receiver}: ${message}`);
+
+    const receiverSocket = onlineUsers.get(receiver);
+    if (receiverSocket) {
+      io.to(receiverSocket).emit("receiveMessage", { sender, message });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    for (const [userId, socketId] of onlineUsers.entries()) {
+      if (socketId === socket.id) {
+        onlineUsers.delete(userId);
+        console.log(`User ${userId} went offline`);
+        break;
+      }
+    }
+    console.log("User disconnected:", socket.id);
+  });
 });
