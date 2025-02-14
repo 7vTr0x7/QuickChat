c435807c3030dde25dd8aa46eac6dbaa3b48b0c4
7vtr0x7 <7vtrox7@gmail.com>
2025-02-15 01:58:15 +0530

    fix: imageUrl added in  user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,6 +6,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
