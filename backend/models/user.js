c52f019d68fbacd30a9aa3213da83f95838d0aad
7vtr0x7 <7vtrox7@gmail.com>
2025-02-15 00:37:13 +0530

    feat: user model

---
+++ backend/models/user.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "QuickChatUser",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("QuickChatUser", UserSchema);
