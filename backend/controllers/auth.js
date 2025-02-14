1665df28f9d1fc550ce9ba519bbe199b2f95dc74
7vtr0x7 <7vtrox7@gmail.com>
2025-02-15 01:18:16 +0530

    feat: sendCookie

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import { User } from "../models/user.js";
 import "dotenv/config";
+import { sendCookie } from "../utils/features.js";
 
 export const register = async (req, res) => {
   try {
@@ -16,19 +16,8 @@ export const register = async (req, res) => {
 
     user = await User.create({ userName, email, password: hashedPassword });
 
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-
     if (user) {
-      res
-        .status(201)
-        .cookie("token", token, {
-          httpOnly: true,
-          maxAge: 7 * 24 * 60 * 60 * 1000,
-        })
-        .json({
-          success: true,
-          message: "Registered Successfully",
-        });
+      sendCookie(user, res, "Register Successfully");
     }
   } catch (error) {
     res
---
+++ backend/utils/features.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const sendCookie = (user, res, message) => {
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+  if (user) {
+    res
+      .status(201)
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
+      .json({
+        success: true,
+        message,
+      });
+  }
+};
