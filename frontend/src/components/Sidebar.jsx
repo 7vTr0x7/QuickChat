52fe28f43ec71ac7f6b574056d64a328e3d3e3ce
7vtr0x7 <7vtrox7@gmail.com>
2025-02-15 16:37:21 +0530

    fix: Title change

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 const Sidebar = ({ setSelectedUser, users }) => {
   return (
     <div className="w-1/4 bg-[#1b1b1b] h-screen p-4 overflow-y-auto border-r border-gray-700">
-      <h2 className="text-xl font-bold text-gray-300 mb-4">QuickChats</h2>
+      <h2 className="text-xl font-bold text-gray-300 mb-4">QuickChat</h2>
       {(users || []).map((user) => (
         <div
           key={user._id}
